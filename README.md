004593f3eeb237bad259515df0e2852cb8322bc1
U-NVIDIA.COM\bqi <bqi@BQI-LT.nvidia.com>
2018-01-18 14:10:21 +0800

    create wmd.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # WMD Wrapper Implementation
 
-This is an implementation of Wrapper for WMD (Word Move Distance) Algorithm based on gensim lib in python.
+This is an implementation of Wrapper for WMD (Word Move Distance) based on gensim lib in python.
 
 ## Requirement
 - Python3 and supporting packages
@@ -9,7 +9,10 @@ This is an implementation of Wrapper for WMD (Word Move Distance) Algorithm base
 ## Usage
 Run `wmd.py` with following args:
 
-docs directories `-l <dir>`, Aggregation threshold `-t <threshold>`. Output file `-o <output.csv>`.
+- Docs directories `-l <testdocs dir>`;
+- Model location `-m <model dir>`;
+- Aggregation threshold `-t <threshold>`;
+- Output file `-o <output.csv>`.
 
 Output:
 Clustered results in specified output file.
@@ -19,4 +22,4 @@ Clustered results in specified output file.
 [From word embeddings to document distances.](https://arxiv.org/pdf/1409.3215.pdf)
 In Proceedings of the 32nd International Conference on International Conference on Machine Learning - Volume 37 (ICML'15), 
 Francis Bach and David Blei (Eds.), Vol. 37. JMLR.org 957-966.
-- [gensim](https://radimrehurek.com/gensim/models/doc2vec.html)
+- [gensim](https://radimrehurek.com/gensim/models/doc2vec.html).
---
+++ wmd.py
@@ -0,0 +1,52 @@
+#!/home/utils/Python-3.6.1/bin/python3
+"""
+A warpped python script to easily aggregate the vectors
+"""
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import optparse
+from gensim.models import Doc2Vec
+
+def GetOptions():
+    parser = optparse.OptionParser()
+    parser.add_option('-l', '--list',
+                      dest="logdir",
+                      help="input docs dir.",
+                      )
+    parser.add_option('-m', '--model',
+                      dest="model",
+                      help="saved model.",
+                      )
+    parser.add_option('-t', '--threshold',
+                      dest="threshold",
+                      help="threshold value.",
+                      )
+    parser.add_option('-o', '--outfile',
+                      dest="outdir",
+                      help="output csv dir.",
+                      )
+    options, remainder = parser.parse_args()
+    return options
+
+
+def get_log_list(logs_dir):
+    """Get all the log file name."""
+    log_list = []
+    for fname in os.listdir(logs_dir):
+        log_list.append(fname)
+    return log_list
+
+
+if __name__ == '__main__':
+    options = GetOptions()
+    logs_dir = options.logdir
+    model_dir = options.model
+    threshold = options.threshold
+    output_dir = options.outdir
+
+    model = Doc2Vec.load(model_dir)
+    unclusted_log_list = get_log_list(logs_dir)
+    cluster_id = 0
